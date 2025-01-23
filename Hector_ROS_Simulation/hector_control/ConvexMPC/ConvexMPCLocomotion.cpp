3211ac55390d22d97218857ddbd891942fb6a934
shimaken-git <shimaken@ba.mbn.or.jp>
2025-01-23 20:40:23 +0900

    update

--- Hector_ROS_Simulation/hector_control/ConvexMPC/ConvexMPCLocomotion.cpp
+++ Hector_ROS_Simulation/hector_control/ConvexMPC/ConvexMPCLocomotion.cpp
@@ -106,12 +106,9 @@ void ConvexMPCLocomotion::run(ControlFSMData &data)
     vBody_des[1] = v_des_world[1];
     vBody_des[2] = 0;
 
-    if(!restart){
-      pBody_RPY_des[0] = 0;
-      pBody_RPY_des[1] = 0;
-      pBody_RPY_des[2] = 0; // seResult.rpy[2];
-      restart = true;
-    }
+    pBody_RPY_des[0] = 0;
+    pBody_RPY_des[1] = 0;
+    pBody_RPY_des[2] = 0; //seResult.rpy[2];
 
     vBody_Ori_des[0] = 0;
     vBody_Ori_des[1] = 0;
